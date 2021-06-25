b8a67c642a5d783469d85590f1f3ca9b95e0bce3
hsmtkk <hsmtkk@users.noreply.github.com>
2021-06-25 13:28:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from scapy.all import *
 
-ether = Ether(src='04:92:26:D7:EC:AC', dst='ff:ff:ff:ff:ff:ff')
-ip = IP(src='0.0.0.0', dst='255.255.255.255')
+ether = Ether(src='08:00:27:4A:50:63', dst='08:00:27:a2:86:76')
+ip = IP(src='10.1.1.100', dst='10.1.1.1')
 udp = UDP(sport=68, dport=67)
 bootp = BOOTP()
 dhcp = DHCP(options=[('message-type', 'inform'), 'end'])
