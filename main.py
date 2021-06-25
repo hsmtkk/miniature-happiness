71d0e33e0fd434a810e0f160402585342e19a15b
admin <admin@localhost>
2021-06-25 03:10:51 +0000

    up

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from scapy.all import *
 ether = Ether(src='04:92:26:D7:EC:AC', dst='ff:ff:ff:ff:ff:ff')
 ip = IP(src='0.0.0.0', dst='255.255.255.255')
 udp = UDP(sport=68, dport=67)
+bootp = BOOTP()
 dhcp = DHCP(options=[('message-type', 'inform'), 'end'])
-packet = ether / ip / udp / dhcp
+packet = ether / ip / udp / bootp / dhcp
 
 packet.show()
 
-send(packet, iface='hoge', verbose=2)
+sendp(packet, verbose=2)
